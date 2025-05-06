f0bdd53d6689f9a4b92aaa8912ae92027d750cd0
Pedro Damián Reyes <damian@ucol.mx>
2025-05-06 10:55:41 -0600

    Agregar archivos de proyecto.

---
+++ Directorio4F/Data/BDDirectorioDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Directorio4F.Data
+{
+    public class BDDirectorioDBContext : DbContext
+    {
+        public BDDirectorioDBContext(
+                   DbContextOptions<BDDirectorioDBContext> options
+                                    ) : base(options)
+        {
+
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
---
+++ Directorio4F/Data/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Directorio4F.Data
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El campo Nombre no puede tener más de 100 caracteres.")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo Correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
+        [StringLength(100, ErrorMessage = "El campo Correo no puede tener más de 100 caracteres.")]
+        public string? Correo { get; set; }
+
+        [Required(ErrorMessage = "El campo Teléfono es obligatorio.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "El campo Teléfono debe contener exactamente 10 dígitos.")]
+        [StringLength(10, ErrorMessage = "El campo Teléfono no puede tener más de 10 caracteres.")]
+        public string? Telefono { get; set; }
+    }
+}
